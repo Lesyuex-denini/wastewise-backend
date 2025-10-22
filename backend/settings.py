c863e0dd41e9732fe432284899d44804c7c7b342
Mila Abadinas <113806288+Lesyuex-denini@users.noreply.github.com>
2025-10-22 08:10:26 +0800

    Add django-cors-headers and enable frontend access

--- backend/settings.py
+++ backend/settings.py
@@ -128,6 +128,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",   
     "http://127.0.0.1:5173",
+    "https://wastewise-website.onrender.com", 
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://wastewise-website.onrender.com"
 ]
 
 import os
